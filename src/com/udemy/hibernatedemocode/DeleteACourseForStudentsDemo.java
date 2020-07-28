c6af899bc4712878186877b20ff1a5466b261158
harika penukonda <penukondaharika@gmail.com>
2020-07-28 15:14:36 -0500

    Create DeleteACourseForStudentsDemo.java

---
+++ src/com/udemy/hibernatedemocode/DeleteACourseForStudentsDemo.java
@@ -0,0 +1,55 @@
+package com.udemy.hibernatedemocode;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.udemy.hibernatedemo.entity.Course;
+import com.udemy.hibernatedemo.entity.Instructor;
+import com.udemy.hibernatedemo.entity.InstructorDetail;
+import com.udemy.hibernatedemo.entity.Review;
+import com.udemy.hibernatedemo.entity.Student;
+
+public class DeleteACourseForStudentsDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			
+			// start a transaction			
+			session.beginTransaction();
+			
+			// get the student course from DB
+			
+			int courseId = 10;
+			Course tempCourse = session.get(Course.class, courseId);
+			
+			// delete the course
+			System.out.println("Deleting course: " + tempCourse);
+			session.delete(tempCourse);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		}finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
